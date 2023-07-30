68287881c1065dcb6969535e2c01e9d0380b1eb7
Kamal Nayan <sknasylum8454@gmail.com>
2023-07-30 13:28:31 +0530

    project-todo-done

---
+++ todo-list/app.js
@@ -0,0 +1,75 @@
+const inputtdl = document.querySelector('.textarea')
+const buttontdl = document.querySelector('.buttoninput')
+const listtdl = document.querySelector('.todolist')
+
+function clickButton(e) {
+    e.preventDefault()
+    addTodo()
+}
+
+// adding todoList
+function addTodo() {
+    const itemall = document.createElement('div')
+    itemall.classList.add('itemall')
+
+    const item = document.createElement('p')
+    item.classList.add('item')
+    item.innerText = inputtdl.value
+    itemall.appendChild(item)
+
+    if (inputtdl.value === '') return
+
+    const checkbutton = document.createElement("button")
+    checkbutton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    checkbutton.classList.add("check-button")
+    itemall.appendChild(checkbutton)
+
+    const trashbutton = document.createElement("button")
+    trashbutton.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    trashbutton.classList.add("trash-button")
+    itemall.appendChild(trashbutton)
+
+    listtdl.appendChild(itemall)
+    inputtdl.value = ''
+}
+
+// checking and delete todoList 
+function okdel(e) {
+    const item = e.target
+
+    // check
+    if (item.classList[0] === 'check-button') {
+        const todolist = item.parentElement
+        todolist.classList.toggle('checklist')
+    }
+
+    // delete
+    if (item.classList[0] === 'trash-button') {
+        const todolist = item.parentElement
+        todolist.remove()
+    }
+}
+
+buttontdl.addEventListener('click', clickButton)
+listtdl.addEventListener('click', okdel)
+
+document.addEventListener('keydown', function(event) {
+    // Check if the pressed key is the Enter key (key code 13)
+    if (event.keyCode === 13 || event.which === 13) {
+      // Find the button with class '.btn'
+      var button = document.querySelector('.buttoninput');
+      if (button) {
+        // Simulate a click on the button
+        button.click();
+      }
+    }
+  });
+
+
+//  @saka_codes
+  
+  
+  
+  
+  
+  
